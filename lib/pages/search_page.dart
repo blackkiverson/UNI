54e882020b78f6e02f04d37dad7d52fc756328d8
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2022-03-17 17:19:07 +0100

    creating a component build for the Search Page

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:smifapp/widgets/like_button.dart';
+import 'package:smifapp/widgets/menu_button.dart';
+import 'package:smifapp/widgets/post_card.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -10,35 +13,82 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  List<Map<String, String>> mockData = [
+    {
+      "username": "Samuel",
+      "tag": "Photography",
+      "post":
+          "Greyhound divisively hello coldly wonderfully marginally far upon excluding.",
+      "profile_image": "img/wptwo.jpg",
+      "post_image": "img/wp-six.jpg",
+      "post_video": "",
+    },
+    {
+      "username": "Isioma",
+      "tag": "Music",
+      "post":
+          "Greyhound divisively hello coldly wonderfully marginally far upon excluding.",
+      "profile_image": "img/wpone.jpg",
+      "post_image": "img/wp-five.jpg",
+      "post_video": "",
+    },
+    {
+      "username": "Miracle",
+      "tag": "Education",
+      "post":
+          "Greyhound divisively hello coldly wonderfully marginally far upon excluding.",
+      "profile_image": "img/wptwo.jpg",
+      "post_image": "",
+      "post_video": "",
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
       children: [
-        SizedBox(height: 10),
-        Padding(
-          padding: EdgeInsets.fromLTRB(55, 0, 0, 0),
-          child: Container(
-            height: 45,
-            width: 300,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Colors.blue.shade100
+        SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(0, 60, 0, 0),
+            child: Column(
+              children: mockData //mockData to generate a dynamic User feed
+                  .map((item) => PostCard(
+                        avatarImage: item["profile_image"]!,
+                        username: item["username"]!,
+                        postTag: item["tag"]!,
+                        postText: item["post"]!,
+                        postImage: item["post_image"]!,
+                        postVideo: item["post_video"],
+                      ))
+                  .toList(),
             ),
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(20, 0, 2, 0),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  suffixIcon:
-                      IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-                  hintText: "Search",
+          ),
+        ),
+        Positioned(
+          top: 0,
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(55, 10, 0, 0),
+            child: Container(
+              height: 45,
+              width: 300,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.blue.shade100),
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(20, 0, 2, 0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    suffixIcon:
+                        IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+                    hintText: "Search",
+                  ),
                 ),
               ),
             ),
           ),
         ),
-        SingleChildScrollView(),
       ],
     );
   }
---
+++ lib/widgets/post_card.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'like_button.dart';
+import 'menu_button.dart';
+
+class PostCard extends StatefulWidget {
+  PostCard(
+      {Key? key,
+      required this.avatarImage,
+      required this.username,
+      required this.postTag,
+      required this.postText,
+      this.postImage,
+      this.postVideo})
+      : super(key: key);
+  final String avatarImage;
+  final String username;
+  final String postTag;
+  final String postText;
+  String? postImage;
+  String? postVideo;
+
+  @override
+  State<PostCard> createState() => _PostCardState();
+}
+
+class _PostCardState extends State<PostCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      margin: const EdgeInsets.fromLTRB(0, 5, 0, 8),
+      child: Column(
+        children: [
+          ListTile(
+            //creates the top padding of every post on the home feed
+            leading: Padding(
+              padding: const EdgeInsets.fromLTRB(3, 0, 0, 0),
+              child: CircleAvatar(
+                radius: 24,
+                backgroundColor: Colors.blue,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.person),
+                ),
+                foregroundImage: AssetImage(widget.avatarImage),
+              ),
+            ),
+
+            //creates the username using the mockData
+            title: Row(
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    widget.username,
+                    style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all(
+                      const Size(0, 0),
+                    ),
+                    padding: MaterialStateProperty.all(const EdgeInsets.all(0)),
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                ),
+              ],
+            ),
+
+            // shows the Mockdata for the Post Tag
+            subtitle: Text(
+              widget.postTag,
+              style: const TextStyle(
+                  color: Colors.black,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 12),
+            ),
+
+            //creating a dropdownbutton with secondary options for the mode menu
+            trailing: MenuButton(),
+          ),
+
+          //uses the Mockdata to generate the post text and media
+          Padding(
+            padding: const EdgeInsets.fromLTRB(75, 0, 10, 20),
+            child: Text(widget.postText,
+                style: const TextStyle(color: Colors.black)),
+          ),
+          widget.postImage != ""&& widget.postImage != null
+              ? Padding(
+                  padding: const EdgeInsets.fromLTRB(75, 0, 20, 0),
+                  child: Image(
+                    image: AssetImage(widget.postImage!),
+                    fit: BoxFit.cover,
+                    height: MediaQuery.of(context).size.height - 550,
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                )
+              : const Padding(padding: EdgeInsets.all(0)),
+
+          //creates the social interaction buttons at the buttom of the post
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              //IconButton() for the comment feature
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.mode_comment_outlined),
+              ),
+
+              //LikeButton() widget
+              LikeButton(),
+
+              //IconButton() for the share feature
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.share_outlined),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
