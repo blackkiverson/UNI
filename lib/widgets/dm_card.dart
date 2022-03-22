95c72b936923991966c888bca8e9209676e8ced5
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2022-03-22 15:20:02 +0100

    updated application ID for firebase

--- lib/widgets/dm_card.dart
+++ lib/widgets/dm_card.dart
@@ -21,63 +21,59 @@ class DmCard extends StatefulWidget {
 class _DmCardState extends State<DmCard> {
   @override
   Widget build(BuildContext context) {
-    return Card(      
+    return Card(
       color: Colors.white,
       clipBehavior: Clip.antiAlias,
       margin: const EdgeInsets.fromLTRB(0, 0.5, 0, 0.4),
-      child: Column(
-        children: [
-          ListTile(
-            //creates the top padding of every post on the home feed
-            leading: Padding(
-              padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
-              child: CircleAvatar(
-                radius: 20,
-                backgroundColor: Colors.blue,
-                child: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.person),
+      child: ListTile(
+        onTap: () {},
+        //creates the top padding of every post on the home feed
+        leading: Padding(
+          padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+          child: CircleAvatar(
+            radius: 20,
+            backgroundColor: Colors.blue,
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.person),
+            ),
+            foregroundImage: AssetImage(widget.avatarImage),
+          ),
+        ),
+
+        //creates the username using the mockData
+        title: Row(
+          children: [
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                widget.username,
+                style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600),
+              ),
+              style: ButtonStyle(
+                minimumSize: MaterialStateProperty.all(
+                  const Size(0, 0),
                 ),
-                foregroundImage: AssetImage(widget.avatarImage),
+                padding: MaterialStateProperty.all(
+                    const EdgeInsets.fromLTRB(0, 0, 10, 10)),
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
               ),
             ),
+            // Text("        " + time),
+          ],
+        ),
 
-            //creates the username using the mockData
-            title: Row(
-              children: [
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    widget.username,
-                    style: const TextStyle(
-                        color: Colors.black,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  style: ButtonStyle(
-                    minimumSize: MaterialStateProperty.all(
-                      const Size(0, 0),
-                    ),
-                    padding: MaterialStateProperty.all(const EdgeInsets.fromLTRB(0, 0, 10, 10)),
-                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  ),
-                ),
-                // Text("        " + time),
-              ],
-            ),
-
-          subtitle: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 0, 10, 5),
-            child: Text(widget.lastDm,
-                style: const TextStyle(color: Colors.black, fontSize: 12)),
-          ),
-            //creating a dropdownbutton with secondary options for the mode menu
-            trailing: MenuButton(),
-          ),
-
-          
-        ],
-      ),      
+        subtitle: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 10, 5),
+          child: Text(widget.lastDm,
+              style: const TextStyle(color: Colors.black, fontSize: 12)),
+        ),
+        //creating a dropdownbutton with secondary options for the mode menu
+        trailing: MenuButton(),
+      ),
     );
   }
 }
