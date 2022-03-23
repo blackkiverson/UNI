7e86a45ab1869116338696614f15c8c514a6ba39
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2022-03-23 01:19:55 +0100

    name change

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:smifapp/main.dart';
+import 'package:uni/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
