dd2c74a6da38ecbf8a4f797bdcd5f21dea13abe4
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2022-04-11 20:57:10 +0100

    ...

--- lib/pages/home/post_page.dart
+++ lib/pages/home/post_page.dart
@@ -13,104 +13,111 @@ class _PostPageState extends State<PostPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //Creating the top elements for page
-      appBar: AppBar(
-        elevation: 1,
-        backgroundColor: Color.fromARGB(255, 12, 12, 12),
-        automaticallyImplyLeading: false,
-        title: TextButton(
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const MainPage(),
-                ));
-          },
-          child: Text("Cancel",
-              style: TextStyle(color: Colors.white)),
-        ),
-        actions: [
-          Align(
-            alignment: Alignment.centerLeft,
-            child: TextButton(
-              onPressed: () {},
-              child: Text("Draft"),
+    return Stack(
+      children: 
+        [Scaffold(
+          //Creating the top elements for page
+          appBar: AppBar(
+            elevation: 1,
+            backgroundColor: Color.fromARGB(255, 12, 12, 12),
+            automaticallyImplyLeading: false,
+            title: TextButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const MainPage(),
+                    ));
+              },
+              child: Text("Cancel",
+                  style: TextStyle(color: Colors.white)),
             ),
+            actions: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text("Draft"),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Submit"),
+                ),
+              ),
+              SizedBox(
+                width: 25,
+              ),
+            ],
           ),
-          Align(
-            alignment: Alignment.centerRight,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text("Submit"),
-            ),
-          ),
-          SizedBox(
-            width: 25,
-          ),
-        ],
-      ),
-
-      //Creating a text field for user to input text
-      body: Column(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height,
-            alignment: Alignment.topCenter,
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 12, 12, 12),
-            ),
-            child: TextField(
-              style: TextStyle(color: Colors.white),
-              minLines: 1,
-              maxLines: null,
-              decoration: InputDecoration(
-                  border: InputBorder.none,
-                  contentPadding:
-                      EdgeInsetsDirectional.fromSTEB(20, 10, 20, 0),
-                  hintText: "Enter Text",
-                  hintStyle:
-                      TextStyle(color: Color.fromARGB(255, 95, 95, 95))),
-            ),
-          ),
-          Container(
-            // constraints: BoxConstraints.expand(height: 60),
-            height: 60,
-            decoration: BoxDecoration(color: Color.fromARGB(255, 12, 12, 12)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    
+          //Creating a text field for user to input text
+          body: SingleChildScrollView(
+            child: Column(
               children: [
-                IconButton(
-                  color: Colors.blue,
-                  onPressed: () {},
-                  icon: Icon(Icons.image_outlined),
-                  iconSize: 30,
+                Container(
+                  // height: MediaQuery.of(context).size.height,
+                  height: MediaQuery.of(context).size.longestSide,
+                  alignment: Alignment.topCenter,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 12, 12, 12),
+                  ),
+                  child: TextField(
+                    style: TextStyle(color: Colors.white),
+                    minLines: 1,
+                    maxLines: null,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding:
+                            EdgeInsetsDirectional.fromSTEB(20, 10, 20, 0),
+                        hintText: "Enter Text",
+                        hintStyle:
+                            TextStyle(color: Color.fromARGB(255, 95, 95, 95))),
+                  ),
                 ),
-                IconButton(
-                  color: Colors.blue,
-                  onPressed: () {},
-                  icon: Icon(Icons.align_horizontal_left),
-                ),
-                IconButton(
-                  color: Colors.blue,
-                  onPressed: () {},
-                  icon: Icon(Icons.insert_link_rounded),
-                ),
-                IconButton(
-                  color: Colors.blue,
-                  onPressed: () {},
-                  icon: Icon(Icons.multitrack_audio_outlined),
-                ),
-                IconButton(
-                  color: Colors.blue,
-                  onPressed: () {},
-                  icon: Icon(Icons.camera_alt_outlined),
+                Container(
+                  // constraints: BoxConstraints.expand(height: 60),
+                  height: 60,
+                  decoration: BoxDecoration(color: Color.fromARGB(255, 12, 12, 12)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        color: Colors.blue,
+                        onPressed: () {},
+                        icon: Icon(Icons.image_outlined),
+                        iconSize: 30,
+                      ),
+                      IconButton(
+                        color: Colors.blue,
+                        onPressed: () {},
+                        icon: Icon(Icons.align_horizontal_left),
+                      ),
+                      IconButton(
+                        color: Colors.blue,
+                        onPressed: () {},
+                        icon: Icon(Icons.insert_link_rounded),
+                      ),
+                      IconButton(
+                        color: Colors.blue,
+                        onPressed: () {},
+                        icon: Icon(Icons.multitrack_audio_outlined),
+                      ),
+                      IconButton(
+                        color: Colors.blue,
+                        onPressed: () {},
+                        icon: Icon(Icons.camera_alt_outlined),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
--- lib/pages/home/search_page.dart
+++ lib/pages/home/search_page.dart
@@ -9,34 +9,16 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
-  List<Map<String, String>> mockData = [
-    {
-      "username": "Samuel",
-      "tag": "Photography",
-      "post":
-          "Greyhound divisively hello coldly wonderfully marginally far upon excluding.",
-      "profile_image": "img/wptwo.jpg",
-      "post_image": "img/wp-six.jpg",
-      "post_video": "",
-    },
-    {
-      "username": "Isioma",
-      "tag": "Music",
-      "post":
-          "Greyhound divisively hello coldly wonderfully marginally far upon excluding.",
-      "profile_image": "img/wpone.jpg",
-      "post_image": "img/wp-five.jpg",
-      "post_video": "",
-    },
-    {
-      "username": "Miracle",
-      "tag": "Education",
-      "post":
-          "Greyhound divisively hello coldly wonderfully marginally far upon excluding.",
-      "profile_image": "img/wptwo.jpg",
-      "post_image": "",
-      "post_video": "",
-    },
+  List<Map<String, String>> mockData = [    
+    // {
+    //   "username": "Miracle",
+    //   "tag": "Education",
+    //   "post":
+    //       "Greyhound divisively hello coldly wonderfully marginally far upon excluding.",
+    //   "profile_image": "img/wptwo.jpg",
+    //   "post_image": "",
+    //   "post_video": "",
+    // },
   ];
 
   @override
