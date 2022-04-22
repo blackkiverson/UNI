9f45c0e226f8ff86a4c1c02dc41ca86b49972fa5
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2022-04-22 19:38:39 +0100

    ...

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,7 +1,34 @@
-class AuthUser {
+// ignore_for_file: non_constant_identifier_names
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AuthUser {
   final String uid;
+  final String name;
+  final int age;
   // final String? email;
 
-  AuthUser({ required this.uid });
+  AuthUser({required this.uid, required this.name, required this.age});
+
+  AuthUser.fromJson(Map<String, Object?> json)
+      : this(
+          uid: json['uid']! as String,
+          name: json['name']! as String,
+          age: json['age']! as int,
+        );
+
+  Map<String, Object?> toJson() {
+    return {
+      'name': name,
+      'age': age,
+    };
+  }
 }
+
+final usersQuery = FirebaseFirestore.instance
+    .collection('users')
+    .orderBy('name')
+    .withConverter<AuthUser>(
+      fromFirestore: (snapshot, _) => AuthUser.fromJson(snapshot.data()!),
+      toFirestore: (user, _) => user.toJson(),
+    );
