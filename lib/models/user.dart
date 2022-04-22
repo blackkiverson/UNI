0b7b993db111c68dce5c0e1d990338cc0a8e9e84
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2022-04-22 22:17:44 +0100

    ...

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,26 +1,56 @@
 // ignore_for_file: non_constant_identifier_names
 
+import 'dart:convert';
+import 'dart:html';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 
 class AuthUser {
   final String uid;
   final String name;
-  final int age;
+  final int follows;
+  final int followers;
+  final String email;
+  final String college;
+  final Image? profilepic;
+  final String? postText;
+  final String? postComment;
+  final Image? postImage;
+  final VideoElement? postVideo;
   // final String? email;
 
-  AuthUser({required this.uid, required this.name, required this.age});
+  AuthUser(
+      {this.postText,
+      this.postComment,
+      this.postImage,
+      this.postVideo,
+      required this.uid,
+      required this.name,
+      required this.follows,
+      required this.followers,
+      required this.college,
+      required this.email,
+      this.profilepic});
 
   AuthUser.fromJson(Map<String, Object?> json)
       : this(
           uid: json['uid']! as String,
           name: json['name']! as String,
-          age: json['age']! as int,
+          follows: json['follows']! as int,
+          followers: json['followers']! as int,
+          email: json['email']! as String,
+          college: json['college']! as String,
+          profilepic: json['profilepic']! as Image,
+          postText: json['postText']! as String,
+          postComment: json['postComment']! as String,
+          postImage: json['postImage']! as Image,
+          postVideo: json['PostVideo']! as VideoElement,
         );
 
   Map<String, Object?> toJson() {
     return {
       'name': name,
-      'age': age,
     };
   }
 }
